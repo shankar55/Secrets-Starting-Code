f2d378a4c6bd3a9b7790fecee0a436813b5527eb
shankar55 <41586715+shankar55@users.noreply.github.com>
2022-03-30 17:59:14 +0530

    Hash function

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const bcrypt =require("bcrypt");
+const saltRounds=10;
+
 const app=express();
 
-console.log(process.env.API_KEY);
+//console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -21,7 +23,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User =new mongoose.model("User",userSchema);
 
@@ -36,19 +37,20 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-  const newUSer=new User({
-    email:req.body.username,
-    password:req.body.password
-  });
-  newUSer.save(function(err){
-    if (err) {
-      console.log(err);
-    }else {
-      res.render("secrets");
-    }
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUSer=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUSer.save(function(err){
+      if (err) {
+        console.log(err);
+      }else {
+        res.render("secrets");
+      }
+    });
 });
-
+});
 app.post("/login",function(req,res){
   const username=req.body.username;
   const password=req.body.password;
@@ -58,9 +60,11 @@ app.post("/login",function(req,res){
       console.log(err);
     }else {
       if(foundUser){
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+    if (result === true) {
+        res.render("secrets");
+    }
+});
       }
     }
   });
